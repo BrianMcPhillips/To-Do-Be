d7ec46866c2e14118b99f40917ecf63a83fe4945
Brian McPhillips <B.McPhillips89@gmail.com>
2021-03-30 22:22:07 -0700

    Find by Id vertical slice

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const List = require('../lib/models/list');
 
 describe('To Do List routes', () => {
   beforeEach(() => {
@@ -21,6 +22,26 @@ describe('To Do List routes', () => {
     
     expect(response.body).toEqual({
       id: expect.any(Number),
+      name: 'clean dishes',
+      type: 'chore',
+      priority: 10,
+      complete: false
+    });
+  });
+
+  it('reads list by id via GET', async() => {
+    const newList = await List.insert({
+      name: 'clean dishes',
+      type: 'chore',
+      priority: 10,
+      complete: false
+    });
+
+    const response = await request(app)
+      .get(`/api/v1/lists/${newList.id}`);
+
+    expect(response.body).toEqual({
+      id: newList.id,
       name: 'clean dishes',
       type: 'chore',
       priority: 10,
